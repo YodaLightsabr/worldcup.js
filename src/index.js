bdcb316fa2ea2b3543ad360bbed480f57de91748
Yoda <yoda@yodacode.xyz>
2022-11-25 17:39:27 -0800

    Export modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = {
     Client: require('./classes/Client.js'),
+    Group: require('./classes/Group.js'),
+    GroupManager: require('./classes/GroupManager.js'),
+    Match: require('./classes/Match.js'),
+    MatchManager: require('./classes/MatchManager.js'),
+    Team: require('./classes/Team.js'),
+    TeamManager: require('./classes/TeamManager.js'),
+    MatchTeam: require('./classes/MatchTeam.js'),
+    MatchTime: require('./classes/MatchTime.js'),
+    MatchWeather: require('./classes/MatchWeather.js')
 }
